14aca35248f0e521f081f672617d0e80e4bfe551
Mynhardt Burger <mynhardt@gmail.com>
2023-05-07 23:03:56 -0400

    Refactor adding relationship field to data types

--- recon/main.py
+++ recon/main.py
@@ -1,42 +1,115 @@
-from typing import NamedTuple
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import Optional
 
 import pandas as pd
 
 
-class SeriesResults(NamedTuple):
-    series_a: pd.Series
-    series_b: pd.Series
+class Relationship(StrEnum):
+    ONE_TO_ONE = "1:1"
+    ONE_TO_MANY = "1:m"
+    MANY_TO_ONE = "m:1"
+    MANY_TO_MANY = "m:m"
 
 
-def uniques(series_a: pd.Series, series_b: pd.Series):
+@dataclass
+class SeriesResults:
+    """Container for left and right series'."""
+
+    left: pd.Series
+    right: pd.Series
+    relationship: Optional[Relationship]
+
+
+@dataclass
+class ReconResults:
+    """Recon results container."""
+
+    common: pd.Series
+    """Left's common items."""
+    left_unique: pd.Series
+    right_unique: pd.Series
+    relationship: Optional[Relationship]
+
+
+def uniques(left: pd.Series, right: pd.Series):
     """
     Filter for unique items which only in one of the series.
 
-    Indexes are preserved.
+    Item order is ignored. Duplicates are preserved. Indexes are preserved.
     """
-    if not isinstance(series_a, pd.Series):
-        raise ValueError("series_a is not a Pandas.Series object")
-    if not isinstance(series_b, pd.Series):
-        raise ValueError("series_a is not a Pandas.Series object")
-
+    left_unique: pd.Series = left[~left.isin(right)]
+    right_unique: pd.Series = right[~right.isin(left)]
     return SeriesResults(
-        series_a=series_a[~series_a.isin(series_b)],
-        series_b=series_b[~series_b.isin(series_a)],
+        left=left_unique,
+        right=right_unique,
+        relationship=None,
     )
 
 
-def common(series_a: pd.Series, series_b: pd.Series):
+def common(left: pd.Series, right: pd.Series):
     """
     Filter for common items which exists in both series.
 
-    Indexes are preserved.
+    Item order is ignored. Duplicates are preserved. Indexes are preserved.
     """
-    if not isinstance(series_a, pd.Series):
-        raise ValueError("series_a is not a Pandas.Series object")
-    if not isinstance(series_b, pd.Series):
-        raise ValueError("series_a is not a Pandas.Series object")
-
+    left_common: pd.Series = left[left.isin(right)]
+    right_common: pd.Series = right[right.isin(left)]
     return SeriesResults(
-        series_a=series_a[series_a.isin(series_b)],
-        series_b=series_b[series_b.isin(series_a)],
+        left=left_common,
+        right=right_common,
+        relationship=possible_relationship(left_common, right_common),
+    )
+
+
+def possible_relationship(left: pd.Series, right: pd.Series):
+    """
+    Determines the possible relationship between `left` and `right`.
+
+    Given the data, determines the most optimistic or tightest relationship possible.
+    """
+    left_unique = left.is_unique
+    right_unique = right.is_unique
+
+    if left_unique and right_unique:
+        return Relationship.ONE_TO_ONE
+    if left_unique and not right_unique:
+        return Relationship.ONE_TO_MANY
+    if not left_unique and right_unique:
+        return Relationship.MANY_TO_ONE
+    return Relationship.MANY_TO_MANY
+
+
+def validate_relationship(
+    left: pd.Series, right: pd.Series, relationship: Relationship
+):
+    """Validates that the relationship between `left` and `right` matches the given
+    `relationship`."""
+    left_unique = left.is_unique
+    right_unique = right.is_unique
+
+    if relationship == Relationship.ONE_TO_ONE:
+        return left_unique and right_unique
+    if relationship == Relationship.ONE_TO_MANY:
+        return left_unique and not right_unique
+    if relationship == Relationship.MANY_TO_ONE:
+        return not left_unique and right_unique
+    if relationship == Relationship.MANY_TO_MANY:
+        return not left_unique and not right_unique
+
+
+def recon(left: pd.Series, right: pd.Series):
+    """
+    Reconciles `left` against `right`.
+
+    Returns the common,  and unique items in left and right.
+    """
+    common_items = common(left, right)
+    unique_items = uniques(left, right)
+
+    return ReconResults(
+        common=common_items.left,
+        left_unique=unique_items.left,
+        right_unique=unique_items.right,
+        relationship=common_items.relationship,
     )
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,33 +1,49 @@
 import pandas as pd
 
-from recon.main import SeriesResults, common, uniques
+from recon.main import Relationship, common, recon, uniques
 
 
 def test_uniques():
     s1 = pd.Series(data=[1, 1, 3, 6])
     s2 = pd.Series(data=[1, 2, 3, 4, 5])
 
-    expected = SeriesResults(
-        series_a=pd.Series(data=[6], index=[3]),
-        series_b=pd.Series(data=[2, 4, 5], index=[1, 3, 4]),
-    )
+    expected_left = pd.Series(data=[6], index=[3])
+    expected_right = pd.Series(data=[2, 4, 5], index=[1, 3, 4])
 
     output = uniques(s1, s2)
 
-    pd.testing.assert_series_equal(expected.series_a, output.series_a)
-    pd.testing.assert_series_equal(expected.series_b, output.series_b)
+    pd.testing.assert_series_equal(expected_left, output.left)
+    pd.testing.assert_series_equal(expected_right, output.right)
+    assert output.relationship is None
 
 
 def test_common():
     s1 = pd.Series([1, 2, 3, 4, 4, 6])
     s2 = pd.Series([1, 3, 3, 4, 5])
 
-    expected = SeriesResults(
-        series_a=pd.Series(data=[1, 3, 4, 4], index=[0, 2, 3, 4]),
-        series_b=pd.Series(data=[1, 3, 3, 4], index=[0, 1, 2, 3]),
-    )
+    expected_left = pd.Series(data=[1, 3, 4, 4], index=[0, 2, 3, 4])
+    expected_right = pd.Series(data=[1, 3, 3, 4], index=[0, 1, 2, 3])
 
     output = common(s1, s2)
 
-    pd.testing.assert_series_equal(expected.series_a, output.series_a)
-    pd.testing.assert_series_equal(expected.series_b, output.series_b)
+    pd.testing.assert_series_equal(expected_left, output.left)
+    pd.testing.assert_series_equal(expected_right, output.right)
+    assert output.relationship == Relationship.MANY_TO_MANY
+
+
+def test_recon():
+    s1 = pd.Series([1, 2, 3])
+    s2 = pd.Series([1, 3, 4])
+
+    expected_common = pd.Series(data=[1, 3], index=[0, 2])
+    expected_left_unique = pd.Series(data=[2], index=[1])
+    expected_right_unique = pd.Series(data=[4], index=[2])
+
+    output = recon(s1, s2)
+
+    pd.testing.assert_series_equal(expected_common, output.common)
+    pd.testing.assert_series_equal(expected_left_unique, output.left_unique)
+    pd.testing.assert_series_equal(expected_right_unique, output.right_unique)
+    assert output.relationship == Relationship.ONE_TO_ONE
+
+    s1._is_copy()
