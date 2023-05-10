7b05a3f8a894bb97c1f043a0f084e05084c83fcc
Mynhardt Burger <mynhardt@gmail.com>
2023-05-09 22:07:41 -0400

    Refactor to class structure

--- recon/main.py
+++ recon/main.py
@@ -1,6 +1,4 @@
-from dataclasses import dataclass
 from enum import StrEnum
-from typing import Optional
 
 import pandas as pd
 
@@ -10,166 +8,117 @@ class Relationship(StrEnum):
     ONE_TO_MANY = "1:m"
     MANY_TO_ONE = "m:1"
     MANY_TO_MANY = "m:m"
+    NONE = ""
 
 
-@dataclass
-class SeriesResults:
-    """Container for left and right series'."""
+class Reconcile:
+    def __init__(self, left: pd.Series, right: pd.Series) -> None:
+        self.left = left.convert_dtypes()
+        self.right = right.convert_dtypes()
+        self.data_map = pd.DataFrame()
+        """
+        index = `left.index`.
+        ["value"] = Combined values from `left` and `right`.
+        ["right_index"] = Mapped `right.index`.
+        """
+        self.commons = pd.DataFrame()
+        self.left_uniques = pd.Series()
+        self.right_uniques = pd.Series()
+        self.relationship: Relationship
 
-    left: pd.Series
-    left_map: pd.Series
-    """`pandas.Series` mapping `left.index` to an Array of right indexes."""
+        self._uniques()
+        self._common()
+        self._possible_relationship()
 
-    right: pd.Series
-    right_map: pd.Series
-    """`pandas.Series` mapping `right.index` to an Array of left indexes."""
+    def _uniques(self, refresh=False):
+        """
+        Filter for unique items which only in one of the series.
 
-    relationship: Optional[Relationship]
+        Item order is ignored. Duplicates are preserved. Indexes are preserved.
+        """
 
+        def to_series_left(x: pd.DataFrame):
+            return pd.Series(
+                data=x["value"].convert_dtypes().values,
+                index=x.index.astype("int64"),
+                name="left_uniques",
+            ).rename_axis(index=None)
 
-@dataclass
-class ReconResults:
-    """Recon results container."""
+        def to_series_right(x: pd.Series):
+            return pd.Series(
+                data=x["value"].convert_dtypes().values,
+                index=x["right_index"].astype("int64"),
+                name="right_uniques",
+            ).rename_axis(index=None)
 
-    left_common: pd.Series
-    left_unique: pd.Series
-    left_map: pd.Series
-    """`pandas.Series` mapping `left.index` to an Array of right indexes."""
+        self._map_data()
 
-    right_common: pd.Series
-    right_unique: pd.Series
-    right_map: pd.Series
-    """`pandas.Series` mapping `right.index` to an Array of left indexes."""
+        if self.left_uniques.empty or refresh:
+            self.left_uniques = to_series_left(
+                self.data_map.loc[self.data_map["right_index"].isna()]
+            )
 
-    relationship: Optional[Relationship]
+        if self.right_uniques.empty or refresh:
+            self.right_uniques = to_series_right(
+                self.data_map.loc[self.data_map.index.isna()]
+            )
 
+    def _common(self, refresh=False):
+        """
+        Filter for common items which exists in both series.
 
-def uniques(left: pd.Series, right: pd.Series):
-    """
-    Filter for unique items which only in one of the series.
+        Complex 1:m, m:1 and m:m relationships can be traced using the `left_map` and
+        `right_map`.
+        Item order is ignored. Duplicates are preserved. Indexes are preserved.
+        """
+        self._map_data()
 
-    Item order is ignored. Duplicates are preserved. Indexes are preserved.
-    """
-    left_unique: pd.Series = left[~left.isin(right)]
-    right_unique: pd.Series = right[~right.isin(left)]
-    left_map = index_map(left_unique, right_unique)
-    right_map = index_map(right_unique, left_unique)
+        if self.commons.empty or refresh:
+            self.commons = (
+                self.data_map.loc[
+                    ~self.data_map.index.isna() & ~self.data_map["right_index"].isna()
+                ]
+                .rename_axis(index="left_index")
+                .convert_dtypes()
+            )
 
-    return SeriesResults(
-        left=left_unique,
-        left_map=left_map,
-        right=right_unique,
-        right_map=right_map,
-        relationship=None,
-    )
+    @staticmethod
+    def _swap_series_data_index(series: pd.Series, series_name: str):
+        return pd.Series(
+            data=series.index.values,
+            index=series.values,
+            name=series_name,
+        ).convert_dtypes()
 
+    def _map_data(self, refresh=False):
+        """
+        Map `left` to `right` through an 'outer' join.
+        """
+        if self.data_map.empty or refresh:
+            self.data_map = (
+                self.left.rename_axis(index="left_index")
+                .to_frame("value")
+                .join(
+                    on="value",
+                    other=self._swap_series_data_index(self.right, "right_index"),
+                    how="outer",
+                )
+                .rename_axis(index="left_index")
+            )
 
-def common(left: pd.Series, right: pd.Series):
-    """
-    Filter for common items which exists in both series.
+    def _possible_relationship(self, refresh=False):
+        """
+        Determines the possible relationship between `left` and `right`.
+        """
 
-    Complex 1:m, m:1 and m:m relationships can be traced using the `left_map` and
-    `right_map`.
-    Item order is ignored. Duplicates are preserved. Indexes are preserved.
-    """
-    left_common: pd.Series = left[left.isin(right)]
-    right_common: pd.Series = right[right.isin(left)]
-    left_map = index_map(left_common, right_common)
-    right_map = index_map(right_common, left_common)
+        left_unique = self.left.is_unique
+        right_unique = self.right.is_unique
 
-    return SeriesResults(
-        left=left_common,
-        left_map=left_map,
-        right=right_common,
-        right_map=right_map,
-        relationship=possible_relationship(left_common, right_common),
-    )
-
-
-def index_map(left: pd.Series, right: pd.Series):
-    """
-    Map the index of `left` to the index of `right`.
-
-    Returns a `pandas.Series`, named 'index_map' where the index=left.index and the
-    data=mapped right.index.
-    """
-    # TODO: replace .apply() with a .merge()/.join() based approach
-    df = left.to_frame(name="left")
-    df["index_map"] = df["left"].apply(lambda x: right.loc[right == x].index.values)
-    return df["index_map"]
-
-
-def possible_relationship(left: pd.Series, right: pd.Series):
-    """
-    Determines the possible relationship between `left` and `right`.
-
-    Given the data, determines the most optimistic or tightest relationship possible.
-    """
-    left_unique = left.is_unique
-    right_unique = right.is_unique
-
-    if left_unique and right_unique:
-        return Relationship.ONE_TO_ONE
-    if left_unique and not right_unique:
-        return Relationship.ONE_TO_MANY
-    if not left_unique and right_unique:
-        return Relationship.MANY_TO_ONE
-    return Relationship.MANY_TO_MANY
-
-
-def validate_relationship(
-    left: pd.Series, right: pd.Series, relationship: Relationship
-):
-    """Validates that the relationship between `left` and `right` matches the given
-    `relationship`."""
-    left_unique = left.is_unique
-    right_unique = right.is_unique
-
-    if relationship == Relationship.ONE_TO_ONE:
-        return left_unique and right_unique
-    if relationship == Relationship.ONE_TO_MANY:
-        return left_unique and not right_unique
-    if relationship == Relationship.MANY_TO_ONE:
-        return not left_unique and right_unique
-    if relationship == Relationship.MANY_TO_MANY:
-        return not left_unique and not right_unique
-
-
-def recon(left: pd.Series, right: pd.Series):
-    """
-    Reconciles `left` against `right`.
-
-    Returns the common,  and unique items in left and right.
-    """
-    common_items = common(left, right)
-    unique_items = uniques(left, right)
-
-    left_map = pd.concat([common_items.left_map, unique_items.left_map])
-    right_map = pd.concat([common_items.right_map, unique_items.right_map])
-
-    return ReconResults(
-        left_common=common_items.left,
-        left_unique=unique_items.left,
-        left_map=left_map,
-        right_common=common_items.right,
-        right_unique=unique_items.right,
-        right_map=right_map,
-        relationship=common_items.relationship,
-    )
-
-
-def apply_index_map(left_common: pd.Series, left_map: pd.Series):
-    """
-    Apply element duplicationa and deduplication using `left_map`.
-
-    This had the effect of growing and shrinking `left_common` for frequency
-    differences between `left` and `right`.
-    The returned `pandas.Series` is equivalent to `right_common`.
-    """
-    # Method chained to contain memory usage
-    exploded_map = left_map.explode().drop_duplicates().dropna().astype("int64")
-
-    # TODO: replace .apply() with a .merge()/.join() based approach
-    return pd.Series(data=exploded_map.index, index=exploded_map.values).map(
-        lambda x: left_common.loc[x]
-    )
+        if left_unique and right_unique:
+            self.relationship = Relationship.ONE_TO_ONE
+        elif left_unique and not right_unique:
+            self.relationship = Relationship.ONE_TO_MANY
+        elif not left_unique and right_unique:
+            self.relationship = Relationship.MANY_TO_ONE
+        else:
+            self.relationship = Relationship.MANY_TO_MANY
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,116 +1,57 @@
 import pandas as pd
+import pytest
 
-from recon.main import Relationship, apply_index_map, common, index_map, recon, uniques
-
-
-def test_index_map():
-    s1 = pd.Series([1, 2, 3])
-    s2 = pd.Series([1, 3, 4])
-
-    output = index_map(s1, s2)
-    expected = pd.Series(data=[[0], [], [1]], index=[0, 1, 2], name="index_map")
-
-    pd.testing.assert_series_equal(output, expected)
+from recon.main import Reconcile, Relationship
 
 
-def test_uniques():
-    s1 = pd.Series(data=[1, 1, 3, 6])
-    s2 = pd.Series(data=[1, 2, 3, 4, 5])
-
-    expected_left = pd.Series(data=[6], index=[3])
-    expected_right = pd.Series(data=[2, 4, 5], index=[1, 3, 4])
-
-    expected_left_map = pd.Series(index=[3], data=[[]], name="index_map")
-    expected_right_map = pd.Series(index=[1, 3, 4], data=[[], [], []], name="index_map")
-
-    output = uniques(s1, s2)
-
-    pd.testing.assert_series_equal(expected_left, output.left)
-    pd.testing.assert_series_equal(expected_right, output.right)
-    pd.testing.assert_series_equal(output.left_map, expected_left_map)
-    pd.testing.assert_series_equal(output.right_map, expected_right_map)
-    assert output.relationship is None
+@pytest.fixture()
+def s1():
+    return pd.Series([1, 1, 2, 3, "text"], name="left")
 
 
-def test_common():
-    s1 = pd.Series([1, 2, 3, 4, 4, 6])
-    s2 = pd.Series([1, 3, 3, 4, 5])
+@pytest.fixture()
+def s2():
+    return pd.Series([1, 2, 2, "text", 4], name="right")
 
-    expected_left = pd.Series(data=[1, 3, 4, 4], index=[0, 2, 3, 4])
-    expected_right = pd.Series(data=[1, 3, 3, 4], index=[0, 1, 2, 3])
 
-    expected_left_map = pd.Series(
-        index=[0, 2, 3, 4], data=[[0], [1, 2], [3], [3]], name="index_map"
+@pytest.fixture()
+def rec(s1, s2):
+    return Reconcile(s1, s2)
+
+
+def test_reconcile_attributes(s1: pd.Series, s2: pd.Series, rec: Reconcile):
+    # Expected data
+    left_uniques = pd.Series(data=[3], index=[3], name="left_uniques").convert_dtypes()
+    right_uniques = pd.Series(
+        data=[4], index=[4], name="right_uniques"
+    ).convert_dtypes()
+    commons = (
+        pd.DataFrame(
+            index=[0, 1, 2, 2, 4],
+            data={"value": [1, 1, 2, 2, "text"], "right_index": [0, 0, 1, 2, 3]},
+        )
+        .rename_axis(index="left_index")
+        .convert_dtypes()
     )
-    expected_right_map = pd.Series(
-        index=[0, 1, 2, 3], data=[[0], [2], [2], [3, 4]], name="index_map"
-    )
-
-    output = common(s1, s2)
-
-    pd.testing.assert_series_equal(output.left, expected_left)
-    pd.testing.assert_series_equal(output.right, expected_right)
-    pd.testing.assert_series_equal(output.left_map, expected_left_map)
-    pd.testing.assert_series_equal(output.right_map, expected_right_map)
-    assert output.relationship == Relationship.MANY_TO_MANY
-
-
-def test_recon():
-    s1 = pd.Series([1, 2, 3])
-    s2 = pd.Series([1, 3, 4])
-
-    expected_left_common = pd.Series(data=[1, 3], index=[0, 2])
-    expected_right_common = pd.Series(data=[1, 3], index=[0, 1])
-    expected_left_unique = pd.Series(data=[2], index=[1])
-    expected_right_unique = pd.Series(data=[4], index=[2])
-
-    expected_left_map = pd.Series(
-        index=[0, 2, 1], data=[[0], [1], []], name="index_map"
-    )
-    expected_right_map = pd.Series(
-        index=[0, 1, 2], data=[[0], [2], []], name="index_map"
+    data_map = (
+        pd.DataFrame(
+            index=[0, 1, 2, 2, 3, 4, pd.NA],
+            data={
+                "value": [1, 1, 2, 2, 3, "text", 4],
+                "right_index": [0, 0, 1, 2, pd.NA, 3, 4],
+            },
+        )
+        .rename_axis(index="left_index")
+        .convert_dtypes()
     )
 
-    output = recon(s1, s2)
+    # Assertions
+    pd.testing.assert_series_equal(s1, rec.left)
+    pd.testing.assert_series_equal(s2, rec.right)
 
-    # Test recon components
-    pd.testing.assert_series_equal(output.left_common, expected_left_common)
-    pd.testing.assert_series_equal(output.right_common, expected_right_common)
-    pd.testing.assert_series_equal(output.left_unique, expected_left_unique)
-    pd.testing.assert_series_equal(output.right_unique, expected_right_unique)
-    pd.testing.assert_series_equal(output.left_map, expected_left_map)
-    pd.testing.assert_series_equal(output.right_map, expected_right_map)
-    assert output.relationship == Relationship.ONE_TO_ONE
+    assert rec.relationship == Relationship.MANY_TO_MANY
+    pd.testing.assert_series_equal(rec.left_uniques, left_uniques)
+    pd.testing.assert_series_equal(rec.right_uniques, right_uniques)
 
-    # Test reconstruction of left and right matches their original
-    pd.testing.assert_series_equal(
-        left=pd.concat([output.left_common, output.left_unique]).sort_index(),
-        right=s1,
-    )
-    pd.testing.assert_series_equal(
-        left=pd.concat([output.right_common, output.right_unique]).sort_index(),
-        right=s2,
-    )
-
-    # Test left + changes = right
-    reconstructed_s2 = pd.concat(
-        [
-            apply_index_map(output.left_common, output.left_map),
-            output.right_unique,
-        ]
-    ).sort_index()
-    pd.testing.assert_series_equal(left=reconstructed_s2, right=s2)
-
-
-def test_apply_index_map():
-    s1 = pd.Series([1, 2, 3, 3])
-    s2 = pd.Series([1, 3, 4, 1])
-
-    common_output = common(s1, s2)
-
-    index_map_output = apply_index_map(s1, common_output.left_map)
-    expected_output = pd.Series(index=[0, 1, 3], data=[1, 3, 1])
-
-    pd.testing.assert_series_equal(
-        index_map_output.sort_index(), expected_output.sort_index()
-    )
+    pd.testing.assert_frame_equal(rec.commons, commons, check_index_type=False)
+    pd.testing.assert_frame_equal(rec.data_map, data_map, check_index_type=False)
