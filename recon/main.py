91b7ccc036f3f0a2ca82446907d6a138cf3bbf25
Mynhardt Burger <mynhardt@gmail.com>
2023-05-16 11:35:39 -0400

    Cleanup

--- recon/main.py
+++ recon/main.py
@@ -1,5 +1,5 @@
 import sys
-from enum import StrEnum
+from enum import Enum
 from functools import cached_property
 from os import PathLike
 from pathlib import PurePath
@@ -27,38 +27,19 @@ RECON_COMPONENTS = Literal[
 ]
 
 
-class Relationship(StrEnum):
-    ONE_TO_ONE = "1:1"
-    ONE_TO_MANY = "1:m"
-    MANY_TO_ONE = "m:1"
-    MANY_TO_MANY = "m:m"
-    NONE = ""
+Relationship = Enum(
+    "Relationship", ["ONE_TO_ONE", "ONE_TO_MANY", "MANY_TO_ONE", "MANY_TO_MANY", "NONE"]
+)
 
 
 class Reconcile:
-    def __init__(
-        self,
-        left: pd.DataFrame,
-        right: pd.DataFrame,
-        left_on: str,
-        right_on: str,
-        suffixes: Suffixes = ("_left", "_right"),
-    ) -> None:
-        self.left = left
-        self.right = right
-        self.left_on = left_on
-        self.right_on = right_on
+    def __init__(self) -> None:
+        self.left: pd.DataFrame
+        self.right: pd.DataFrame
+        self.left_on: str
+        self.right_on: str
 
-        if isinstance(self.left, pd.Series):
-            self.left = self.left.to_frame(name="left")
-            self.left_on = "left"
-        if isinstance(self.right, pd.Series):
-            self.right = self.right.to_frame(name="right")
-            self.right_on = "right"
-
-        self.suffixes = suffixes
-
-        self._left_name_map, self._right_name_map = self._map_column_names()
+        self.suffixes: tuple[str, str]
 
         self.output_dispatch: dict[str, Union[pd.Series, pd.DataFrame]] = {
             "left_only": self.left_only,
@@ -73,8 +54,41 @@ class Reconcile:
             "all": self.all_data,
         }
 
+    def _map_column_names(self):
+        left_columns = set(self.left.reset_index(names="index").columns)
+        right_columns = set(self.right.reset_index(names="index").columns)
+        common_columns = left_columns & right_columns
+        left_only = left_columns - right_columns
+        right_only = right_columns - left_columns
+
+        # Where the merge on column has the same name pandas doesn't add a suffix
+        if self.left_on == self.right_on:
+            common_columns.remove(self.left_on)
+            left_columns.add(self.left_on)
+            right_columns.add(self.right_on)
+
+        if len(self.suffixes) == 1:
+            suffixes = (self.suffixes[0], "")
+        assert self.suffixes[0] or self.suffixes[1]
+        suffixes = (
+            self.suffixes[0] or "",
+            self.suffixes[1] or "",
+        )
+
+        left_map: dict[str, str] = {}
+        left_map.update({col: col for col in left_only})
+        left_map.update({col: col + suffixes[0] for col in common_columns})
+
+        right_map: dict[str, str] = {}
+        right_map.update({col: col for col in right_only})
+        right_map.update({col: col + suffixes[1] for col in common_columns})
+
+        return left_map, right_map
+
     @cached_property
     def all_data(self) -> pd.DataFrame:
+        self._left_name_map, self._right_name_map = self._map_column_names()
+
         return pd.merge(
             self.left.reset_index(names="index"),
             self.right.reset_index(names="index"),
@@ -144,37 +158,6 @@ class Reconcile:
         else:
             return Relationship.MANY_TO_MANY
 
-    def _map_column_names(self):
-        left_columns = set(self.left.reset_index(names="index").columns)
-        right_columns = set(self.right.reset_index(names="index").columns)
-        common_columns = left_columns & right_columns
-        left_only = left_columns - right_columns
-        right_only = right_columns - left_columns
-
-        # Where the merge on column has the same name pandas doesn't add a suffix
-        if self.left_on == self.right_on:
-            common_columns.remove(self.left_on)
-            left_columns.add(self.left_on)
-            right_columns.add(self.right_on)
-
-        if len(self.suffixes) == 1:
-            suffixes = (self.suffixes[0], "")
-        assert self.suffixes[0] or self.suffixes[1]
-        suffixes = (
-            self.suffixes[0] or "",
-            self.suffixes[1] or "",
-        )
-
-        left_map: dict[str, str] = {}
-        left_map.update({col: col for col in left_only})
-        left_map.update({col: col + suffixes[0] for col in common_columns})
-
-        right_map: dict[str, str] = {}
-        right_map.update({col: col for col in right_only})
-        right_map.update({col: col + suffixes[1] for col in common_columns})
-
-        return left_map, right_map
-
     def info(self) -> None:
         left_stats = (
             f"{len(self.left_both):,d} common + "
@@ -224,6 +207,7 @@ class Reconcile:
             self.output_dispatch[component].to_csv(
                 sys.stdout, index_label="index", **kwargs
             )
+            print("--------- END ----------")
 
     @staticmethod
     def _read_file(
